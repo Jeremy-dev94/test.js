96ca23e1f0e713ca05098e7887662acc6d5737ea
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-07-04 03:29:44 +0200

    test.js

--- test.js
+++ test.js
@@ -1,29 +1,44 @@
-let message ;
-let chaineTv = 1;
+const el = document.getElementById('register-form');
+el.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-switch (chaineTv){
-    case 1 :
-        message = "TF1";
-        break;
-    case 2 :
-        message = "France2";
-        break;
-    case 3 :
-        message = "France3";
-        break;
-    case 4 :
-        message = "Canal+";
-        break;
-    case 5 : 
-        message = "France5";
-        break;
-    case 6 : 
-        message = "M6";
-        break;
-    case 7 : 
-        message = "Arte";
-    break;
-    default : 
-        message = "chaine-inconnue";
+  let civility = event.target.female.checked ? 'female' : 'male';
+  let name = event.target.name.value;
+  let category = event.target.category.value;
+  let newsletter = event.target.newsletter.checked;
+
+  // Les variables déclarées ci-dessus contiennent les données du formulaire
+
+
+let displayCivility = civility === 'female' ? "Mme" : "M";
+let newsletterMessage
+
+if (newsletter) {
+    newsletterMessage = "Merci de vous etre abonné a notre newsletter ! " ;
+} else {
+    newsletterMessage = "Abonné vous vite a notre newsletter !";
 }
-console.log(message);
+
+let displayCat;
+
+switch (category){
+    case 'starter' :
+        displayCat = "entrée froide";
+        break;
+    case 'soup' :
+        displayCat = "soupe";
+        break;
+    case 'main-course' :
+        displayCat = "salade";
+        break;
+    case 'fruit-dessert' :
+        displayCat = "salade de fruit";
+        break;
+    case 'chocolate-dessert' :
+        displayCat = "chocolate" ;
+        break;
+    default :
+    displayCat = "entrée-froide" ;
+}
+    alert(`Bonjour ${displayCivility} ${name}. ${newsletterMessage}, elle permettra de vous envoyer de délicieuses recettes de votre catégorie préférée, ${displayCat}!`)
+});
